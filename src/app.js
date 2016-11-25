31f4d1da80e031678ab4c3d7e91dc48066d5b9c3
larfoley <lar_x87@yahoo.com>
2016-11-25 00:02:04 +0000

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ app.set('views', __dirname + '/views');
 
 // Home route
 app.get('/', function(req, res) {
-  const toggle-nav = require('toggle-nav');
   res.render("index", {title: "Home", scripts: ["/js/loading-screen.js"]});
 
 });
