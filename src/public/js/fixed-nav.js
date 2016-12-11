d87482ecf5ba39da8feb48e1ff6efae0cb16edc5
larfoley <lar_x87@yahoo.com>
2016-12-11 14:34:06 +0000

    optimised fixed nav

--- src/public/js/fixed-nav.js
+++ src/public/js/fixed-nav.js
@@ -2,85 +2,87 @@
 
   // make navigation fixed before the nav leaves the viewport
 
-  var body = document.body,
-      sideNav = document.querySelector('.page-nav'),
-      sideNavOffset = new Number(sideNav.offsetTop),
-      sideNavLinks = sideNav.querySelector('a');
+  if (document.querySelector('.page-nav')) {
 
-  function scrollToEl(el, options) {
+    var body = document.body,
+    sideNav = document.querySelector('.page-nav'),
+    sideNavOffset = new Number(sideNav.offsetTop),
+    sideNavLinks = sideNav.querySelector('a');
 
-    var id;
+    function scrollToEl(el, options) {
 
+      var id;
 
-    if (!options) {
-      options = {
-        speed: 10
-      };
+      if (!options) {
+        options = {
+          speed: 10
+        };
+      }
+
+      clearInterval(id);
+
+      id = setInterval(function() {
+
+        var scrollDirection = 1;
+
+        if (body.scrollTop > el.offsetTop) {
+          scrollDirection = 1;
+          body.scrollTop -= 10;
+          console.log("Scrolling up");
+        } else if(body.scrollTop < el.offsetTop) {
+          body.scrollTop += 10;
+          scrollDirection = 0;
+          console.log("Scrolling down");
+        }
+
+        if (body.scrollTop === el.offsetTop) {
+          console.log("Stop scrolling");
+          clearInterval(id);
+        }
+
+      }, options.speed);
     }
 
-    clearInterval(id);
+    // Bind handlers
 
-    id = setInterval(function() {
+    sideNav.onclick = function(e) {
 
-      var scrollDirection = 1;
+      // e.preventDefault();
 
-      if (body.scrollTop > el.offsetTop) {
-        scrollDirection = 1;
-        body.scrollTop -= 10;
-        console.log("Scrolling up");
-      } else if(body.scrollTop < el.offsetTop) {
-        body.scrollTop += 10;
-        scrollDirection = 0;
-        console.log("Scrolling down");
+      var href = e.target.href || false;
+      var id = "";
+
+      if (href) {
+
+        var parseHref = false;
+
+        for (var i = 0; i < href.length; i++) {
+          if (href[i] === "#") {
+            parseHref = true;
+          }
+          if (parseHref) {
+            id += href[i];
+          }
+        }
+
+        // scrollToEl(document.querySelector(id), {
+        //   speed: 1
+        // });
+
+      }
+    };
+
+    window.addEventListener('scroll', function() {
+
+      if (sideNavOffset < body.scrollTop) {
+        sideNav.style.position = "fixed";
+        sideNav.style.top = 0;
+      } else {
+        sideNav.style.position = "static";
       }
 
-      if (body.scrollTop === el.offsetTop) {
-        console.log("Stop scrolling");
-        clearInterval(id);
-      }
-
-    }, options.speed);
+    }, false);
   }
-
-  // Bind handlers
-
-  sideNav.onclick = function(e) {
-
-    // e.preventDefault();
-
-    var href = e.target.href || false;
-    var id = "";
-
-    if (href) {
-
-      var parseHref = false;
-
-      for (var i = 0; i < href.length; i++) {
-        if (href[i] === "#") {
-          parseHref = true;
-        }
-        if (parseHref) {
-          id += href[i];
-        }
-      }
-
-      // scrollToEl(document.querySelector(id), {
-      //   speed: 1
-      // });
-
-    }
-  };
-
-  window.addEventListener('scroll', function() {
-
-    if (sideNavOffset < body.scrollTop) {
-      sideNav.style.position = "fixed";
-      sideNav.style.top = 0;
-    } else {
-      sideNav.style.position = "static";
-    }
-
-  }, false);
 
 
 }());
