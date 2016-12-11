07f111af41fce42d302b31d29675a6eb6529153b
larfoley <lar_x87@yahoo.com>
2016-12-11 14:35:54 +0000

    optimised loading screen

--- src/public/js/loading-screen.js
+++ src/public/js/loading-screen.js
@@ -1,15 +1,15 @@
 (function() {
 
-
   window.onload = function() {
 
     var loadingEl = document.querySelector(".loading-screen");
-
+    var body = document.body;
+    
     loadingEl.style.opacity = 0;
 
     setTimeout(function() {
 
-      document.body.removeChild(loadingEl);
+      body.removeChild(loadingEl);
 
     },1000);
   };
